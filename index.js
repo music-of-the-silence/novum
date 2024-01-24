c23196fdfdcf0ee27acaa72b451cc418cf3c23ce
Chandan Nath <144007351+music-of-the-silence@users.noreply.github.com>
2024-01-24 19:09:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#element', {
+    strings: ['for professionals - bespoke portfolios for doctors, designers, and advocates.', 'in e-commerce - creating seamless online retail experiences that drive sales.', 'in education - inspiring learning through websites for schools, colleges, and coaching centers.', 'in healthcare - building a robust digital presence for nursing homes and medical colleges.', 'across industries - tailored web development that elevates diverse brand identities.'],
+    typeSpeed: 150,
+  });
